768d35be42d7b5d0884daac1c49b3c4173ad69cf
Melody Cherry <melody.laffitau@gmail.com>
2024-11-25 13:39:53 +0100

    Modif 25 nov - 13h00

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: mlaffita <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 17:49:22 by mlaffita          #+#    #+#             */
-/*   Updated: 2024/11/24 20:58:06 by mlaffita         ###   ########.fr       */
+/*   Updated: 2024/11/25 13:36:28 by mlaffita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
 
 char	*get_next_line(int fd)
 {
@@ -42,59 +43,27 @@ char	*get_next_line(int fd)
 			buffer[0] = '\0';
 			if (rd == 0)
 			{
-				return (line);
+				if (line && *line != '\0')
+					return (line);
+				free(line);
+				return (NULL);
 			}
-			
 	}
 }
+
 /*
-char *get_next_line(int fd)
+#include <stdio.h>
+#include <fcntl.h>
+int	main (int ac, char **av)
 {
-    int         rd;
-    static char buffer[BUFFER_SIZE + 1];
-    char        *n_buff;
-    char        *line;
+	int 	fd;
+	char	*line;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    line = NULL;
-    while (1)
-    {
-        if (buffer[0] == '\0') // Si le buffer est vide, lire depuis le fichier
-        {
-            rd = read(fd, buffer, BUFFER_SIZE);
-            if (rd == -1) // Erreur de lecture
-                return (NULL);
-            if (rd == 0) // Fin de fichier
-                return (line); // Retourne la ligne accumulée ou NULL si vide
-            buffer[rd] = '\0'; // Terminer le buffer lu
-        }
-        n_buff = ft_strchr(buffer, '\n'); // Cherche un '\n' dans le buffer
-        if (n_buff) // '\n' trouvé
-        {
-            line = ft_strjoin(line, ft_strndup(buffer, n_buff - buffer + 1));
-            ft_strlcpy(buffer, n_buff + 1, BUFFER_SIZE - (n_buff - buffer));
-            return (line);
-        }
-        else // Pas de '\n', ajouter tout le buffer à la ligne
-        {
-            line = ft_strjoin(line, buffer);
-            buffer[0] = '\0'; // Vider le buffer
-        }
-    }
+	fd = open(*(av + 1), O_RDONLY);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free (line);
+	}
 }
-
-// #include <stdio.h>
-// #include <fcntl.h>
-// int	main (int ac, char **av)
-// {
-// 	int 	fd;
-// 	char	*line;
-
-// 	fd = open(*(av + 1), O_RDONLY);
-// 	while ((line = get_next_line(fd)) != NULL)
-// 	{
-// 		printf("%s", line);
-// 		free (line);
-// 	}
-// }
+*/
